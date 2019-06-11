63fc9596fae54b25971ce6b9c7ae1d2815f99e8b
Jeff Kapochus <jeff.kapochus@cerner.com>
2019-06-11 08:52:28 -0400

    Added Some Constants for the Front-End To Use, Will Expand

---
+++ src/main/client/src/App/Utils/Constants.jsx
@@ -0,0 +1,6 @@
+export const STATUS_OK = 200;
+export const HTTP_GET = "GET";
+export const HTTP_POST = "POST";
+export const APP_URL = "http://localhost:8080/";
+export const SUBMISSION_LIST_URL = "submission";
+export const SUBMISSION_URL = "submission/";
